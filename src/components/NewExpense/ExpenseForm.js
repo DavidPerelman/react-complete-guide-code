78a817f3066752d5aecd79d7920c78a2b73679a6
David Perelman <dperelman3@gmail.com>
2022-12-26 12:22:51 +0200

    Handling Form Submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,15 +22,33 @@ const ExpenseForm = () => {
   const amountChangeHandler = (e) => {
     setEnteredAmount(e.target.value);
     // setUserInput({ ...userInput, enteredAmount: e.target.value });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: e.target.value };
+    // });
   };
 
   const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
     // setUserInput({ ...userInput, enteredDate: e.target.value });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: e.target.value };
+    // });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    <form onSubmit={(e) => submitHandler(e)}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
